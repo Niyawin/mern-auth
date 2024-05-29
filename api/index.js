5bddc1be19128c055253ba667995e2322fe958ad
Sandhiya <sandy.gacc@gmail.com>
2024-05-29 19:57:29 +0530

    create sign-up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_OLD_VERSION).then(() =>{
@@ -11,10 +12,12 @@ mongoose.connect(process.env.MONGO_OLD_VERSION).then(() =>{
 })
 
 const app = express();
+app.use(express.json());
 
 app.listen(3010, () =>{
     console.log('Server Listening on port 3010')
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
